1669f537765987cf201c0730deeeec1bff67daf1
Steven Melville <stevenmelville@SUU.EDU>
2019-10-10 16:16:12 -0600

    Added zombie code

---
+++ zombieBattle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int numZombies = 3, numRooms = 3, regenRate = 3;
+	const char STUNNED;
+	char * zombies[numZombies];
+	char rooms[numRooms];
+	bool zombiesAlive = true;
+	
+	while(zombiesAlive){
+		
+		
+		zombiesAlive = false;
+		for(int i = 0;  i < numZombies; i++{
+			if(zombies[i] != &STUNNED){
+				 zombiesAlive = true;
+			}
+		}
+	}
+
+	
+}
+
